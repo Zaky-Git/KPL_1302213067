ce69ee1ff9c4a722e3c2355d50121f1b39d6723e
MUHAMMAD ZAKY FATHURAHIM <mzakyf5503@gmail.com>
2023-03-04 18:10:34 +0700

    tambahan fitur GUI

--- tpmodul3_1302213067/Form1.cs
+++ tpmodul3_1302213067/Form1.cs
@@ -12,6 +12,38 @@ namespace tpmodul3_1302213067
 {
     public partial class Form1 : Form
     {
+        public Form1()
+        {
+            InitializeComponent();
 
+            button1.Text = "Submit/Kirim";
+
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.BackColor = Color.White;
+
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.Dock = DockStyle.None;
+
+
+        }
+
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.AcceptButton = button1;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Halo " + textBox1.Text;
+        }
     }
 }
